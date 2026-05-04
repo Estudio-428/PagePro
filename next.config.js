f3898175ffbf75cb75a6a3e2cd2905eca7f2b242
Leandro Oliveira <leandrooliveiradg@gmail.com>
2026-05-04 08:45:56 -0300

    fix: use http redirects for legacy routes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const path = require('path');
 
 const nextConfig = {
   output: 'standalone',
+  async redirects() {
+    return [
+      { source: '/', destination: '/products', permanent: false },
+      { source: '/settings', destination: '/products', permanent: false },
+      { source: '/dashboard/settings', destination: '/products', permanent: false },
+      { source: '/dashboard/products', destination: '/products', permanent: false },
+      { source: '/dashboard/import', destination: '/import', permanent: false },
+      { source: '/dashboard/editor', destination: '/editor', permanent: false },
+    ];
+  },
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
   },
